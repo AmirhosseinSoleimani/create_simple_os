c55dd69dcc846f5a1772295032ce5476e51224ef
Amirhossein <Ah.Soleimani1313@gmail.com>
2024-12-16 02:44:27 -0500

    feat(bootloader): implement a simple bootloader in assembly

---
+++ bootloader/bootloader.asm
@@ -0,0 +1,17 @@
+
+
+[bits 16]
+[org 0x7c00]
+
+start:
+    mov ah, 0x0E 
+    mov al, 'H'
+    int 0x10 
+    mov al, 'i'
+    int 0x10
+
+    cli
+    hlt
+
+times 510 - ($-$$) db 0
+dw 0xAA55    
